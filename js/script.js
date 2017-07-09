ccbafdad32551dcc2738d5fec57b9492a0431d36
Chris Halliwell <zultan.zul@gmail.com>
2017-07-09 22:58:29 +0100

    Clean

--- js/script.js
+++ js/script.js
@@ -504,157 +504,13 @@ var AirPlane = function() {
 	this.mesh.add(suspension);
 };
 
-var Fox = function() {
-	
-	this.mesh = new THREE.Object3D();
-	
-	var redFurMat = new THREE.MeshPhongMaterial({color:Colors.red, shading:THREE.FlatShading});
-
-	// Create the Body
-	var geomBody = new THREE.BoxGeometry(100,50,50,1,1,1);
-	var body = new THREE.Mesh(geomBody, redFurMat);
-	body.castShadow = true;
-	body.receiveShadow = true;
-	this.mesh.add(body);
-	
-	// Create the Chest
-	var geomChest = new THREE.BoxGeometry(50,60,70,1,1,1);
-	var chest = new THREE.Mesh(geomChest, redFurMat);
-	chest.position.x = 60;
-	chest.castShadow = true;
-	chest.receiveShadow = true;
-	this.mesh.add(chest);
-
-	// Create the Head
-	var geomHead = new THREE.BoxGeometry(40,55,50,1,1,1);
-	this.head = new THREE.Mesh(geomHead, redFurMat);
-	this.head.position.set(80, 35, 0);
-	this.head.castShadow = true;
-	this.head.receiveShadow = true;
-
-	// Create the Snout
-	var geomSnout = new THREE.BoxGeometry(40,30,30,1,1,1);
-	var snout = new THREE.Mesh(geomSnout, redFurMat);
-	geomSnout.vertices[0].y-=5;
-	geomSnout.vertices[0].z+=5;
-	geomSnout.vertices[1].y-=5;
-	geomSnout.vertices[1].z-=5;
-	geomSnout.vertices[2].y+=5;
-	geomSnout.vertices[2].z+=5;
-	geomSnout.vertices[3].y+=5;
-	geomSnout.vertices[3].z-=5;
-	snout.castShadow = true;
-	snout.receiveShadow = true;
-	snout.position.set(30,0,0);
-	this.head.add(snout);
-
-	// Create the Nose
-	var geomNose = new THREE.BoxGeometry(10,15,20,1,1,1);
-	var matNose = new THREE.MeshPhongMaterial({color:Colors.brown, shading:THREE.FlatShading});
-	var nose = new THREE.Mesh(geomNose, matNose);
-	nose.position.set(55,0,0);
-	this.head.add(nose);
-
-	// Create the Ears
-	var geomEar = new THREE.BoxGeometry(10,40,30,1,1,1);
-	var earL = new THREE.Mesh(geomEar, redFurMat);
-	earL.position.set(-10,40,-18);
-	this.head.add(earL);
-	earL.rotation.x=-Math.PI/10;
-	geomEar.vertices[1].z+=5;
-	geomEar.vertices[4].z+=5;
-	geomEar.vertices[0].z-=5;
-	geomEar.vertices[5].z-=5;
-
-	// Create the Ear Tips
-	var geomEarTipL = new THREE.BoxGeometry(10,10,20,1,1,1);
-	var matEarTip = new THREE.MeshPhongMaterial({color:Colors.white, shading:THREE.FlatShading});
-	var earTipL = new THREE.Mesh(geomEarTipL, matEarTip);
-	earTipL.position.set(0,25,0);
-	earL.add(earTipL);
-
-	var earR = earL.clone();
-	earR.position.z = -earL.position.z;
-	earR.rotation.x = -	earL.rotation.x;
-	this.head.add(earR);
-
-	this.mesh.add(this.head);
-
-	
-	// Create the tail
-	var geomTail = new THREE.BoxGeometry(80,40,40,2,1,1);
-	geomTail.vertices[4].y-=10;
-	geomTail.vertices[4].z+=10;
-	geomTail.vertices[5].y-=10;
-	geomTail.vertices[5].z-=10;
-	geomTail.vertices[6].y+=10;
-	geomTail.vertices[6].z+=10;
-	geomTail.vertices[7].y+=10;
-	geomTail.vertices[7].z-=10;
-	this.tail = new THREE.Mesh(geomTail, redFurMat);
-	this.tail.castShadow = true;
-	this.tail.receiveShadow = true;
-
-	// Create the tail Tip
-	var geomTailTip = new THREE.BoxGeometry(20,40,40,1,1,1);
-	var matTailTip = new THREE.MeshPhongMaterial({color:Colors.white, shading:THREE.FlatShading});
-	var tailTip = new THREE.Mesh(geomTailTip, matTailTip);
-	tailTip.position.set(80,0,0);
-	tailTip.castShadow = true;
-	tailTip.receiveShadow = true;
-	this.tail.add(tailTip);
-	this.tail.position.set(-40,10,0);
-	geomTail.translate(40,0,0);
-	geomTailTip.translate(10,0,0);
-	this.tail.rotation.z = Math.PI/1.5;
-	this.mesh.add(this.tail);
-
-
-	// Create the Legs
-	var geomLeg = new THREE.BoxGeometry(20,60,20,1,1,1);
-	this.legFR = new THREE.Mesh(geomLeg, redFurMat);
-	this.legFR.castShadow = true;
-	this.legFR.receiveShadow = true;
-
-	// Create the feet
-	var geomFeet = new THREE.BoxGeometry(20,20,20,1,1,1);
-	var matFeet = new THREE.MeshPhongMaterial({color:Colors.white, shading:THREE.FlatShading});
-	var feet = new THREE.Mesh(geomFeet, matFeet);
-	feet.position.set(0,0,0);
-	feet.castShadow = true;
-	feet.receiveShadow = true;
-	this.legFR.add(feet);
-	this.legFR.position.set(70,-12,25);
-	geomLeg.translate(0,40,0);
-	geomFeet.translate(0,80,0);
-	this.legFR.rotation.z = 16;
-	this.mesh.add(this.legFR);
-
-	this.legFL = this.legFR.clone();
-	this.legFL.position.z = -this.legFR.position.z;
-	this.legFL.rotation.z = -this.legFR.rotation.z;
-	this.mesh.add(this.legFL);
-
-	this.legBR = this.legFR.clone();
-	this.legBR.position.x = -(this.legFR.position.x)+50;
-	this.legBR.rotation.z = -this.legFR.rotation.z;
-	this.mesh.add(this.legBR);
-
-	this.legBL = this.legFL.clone();
-	this.legBL.position.x = -(this.legFL.position.x)+50;
-	this.legBL.rotation.z = -this.legFL.rotation.z;
-	this.mesh.add(this.legBL);
-
-};
-
-
 var sky;
 var forest;
 var land;
 var orbit;
 var airplane;
 var sun;
-var fox;
+
 
 var mousePos={x:0, y:0};
 var offSet = -600;
@@ -700,14 +556,6 @@ function createPlane(){
 	// airplane.mesh.rotation.z = Math.PI/15;
 	scene.add(airplane.mesh);
 }
-
-function createFox(){ 
-	fox = new Fox();
-	fox.mesh.scale.set(.35,.35,.35);
-	fox.mesh.position.set(-40,110,-250);
-	scene.add(fox.mesh);
-}
-
 
 function updatePlane() {
 
@@ -766,7 +614,7 @@ function init(event) {
 	createLand();
 	createForest();
 	createSky();
-	//createFox();
+
 
 	document.addEventListener('mousemove', handleMouseMove, false);
 
